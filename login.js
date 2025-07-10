f41d2c1639e5bd98d206fd5d5b73eafebc9d993b
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-09 22:56:45 -0300

    A006: seccond loke at a js file

--- login.js
+++ login.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const router = express.Router();
-const mongoose = require('mongoose');
-const AccountModel = mongoose.model('Account');
 
 module.exports = () => {
     router.post('/signup', new SignUpRouter().route)
 };
+
+// singup-router.js
+const express = require('express');
+const router = express.Router();
 
 class SignUpRouter {
     async route (req, res) {
@@ -14,6 +14,11 @@ class SignUpRouter {
         return res.status(400).json({ error: 'password must be equal to repeatPassword' })
     }
 }
+
+
+// singup-usecase.js
+const mongoose = require('mongoose');
+const AccountModel = mongoose.model('Account');
 
 class SignUpUseCase {
     async signUp(email, password, repeatPassword) {
