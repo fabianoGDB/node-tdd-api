8140c20ee3446279aa33c979c8f1ca592a593844
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-09 22:56:18 -0300

    A005: first impression of a js file

---
+++ login.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+const AccountModel = mongoose.model('Account');
+
+module.exports = () => {
+    router.post('/signup', new SignUpRouter().route)
+};
+
+class SignUpRouter {
+    async route (req, res) {
+        const { email, password, repeatPassword } = req.body;
+        new SignUpUseCase().signUp(email, password, repeatPassword)
+        return res.status(400).json({ error: 'password must be equal to repeatPassword' })
+    }
+}
+
+class SignUpUseCase {
+    async signUp(email, password, repeatPassword) {
+        if (password === repeatPassword) {
+            const user = await AccountModel.create({ email, password });
+            return user;
+        }
+    }
+}
