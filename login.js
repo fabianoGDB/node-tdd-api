49e93c16d0894744c87dfbcb650bcfc5096b2a16
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-10 23:30:34 -0300

    A007: repository logic

--- login.js
+++ login.js
@@ -1,4 +1,3 @@
-
 module.exports = () => {
     router.post('/signup', new SignUpRouter().route)
 };
@@ -17,14 +16,21 @@ class SignUpRouter {
 
 
 // singup-usecase.js
-const mongoose = require('mongoose');
-const AccountModel = mongoose.model('Account');
-
 class SignUpUseCase {
     async signUp(email, password, repeatPassword) {
         if (password === repeatPassword) {
-            const user = await AccountModel.create({ email, password });
-            return user;
+            new AddAccountRepository().add(email, password);
         }
+    }
+}
+
+// add-acount-repository.js
+const mongoose = require('mongoose');
+const AccountModel = mongoose.model('Account');
+
+class AddAccountRepository {
+    async add(email, password) {
+        const user = await AccountModel.create({ email, password });
+        return user;
     }
 }
