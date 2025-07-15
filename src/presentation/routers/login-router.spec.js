2ab1c5d97b72b88664ed5f8ba4d711c61bb9be5a
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-15 19:10:17 -0300

    A013: adding test for valid and invalid credentials

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,14 @@
 class LoginRouter{
   route (httpRequest) {
-    if(!httpRequest.body.email){
+    const {email, password } = httpRequest.body
+    if(!email || !password){
       return {
         statusCode: 400
       }
+    }
+
+    return {
+      statusCode: 200
     }
   }
 }
@@ -18,5 +23,29 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+  });
+
+  test('should return 400 on no password provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: '123@email.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  });
+
+
+  test('should return 200 on all credentials valid', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: '123@email.com',
+        password: '123123'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
   });
 });
