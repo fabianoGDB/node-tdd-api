b87c3dd050b67d766036b86693e8391895f1bf13
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-15 18:55:00 -0300

    A012: tdd for login router

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,4 +1,22 @@
+class LoginRouter{
+  route (httpRequest) {
+    if(!httpRequest.body.email){
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
 describe('Login Router', () => {
-  test('should return 200 on valid credentials', () => {
+  test('should return 400 on no email provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: '123123'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
   });
 });
