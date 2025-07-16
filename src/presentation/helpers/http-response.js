5b6dbe8f4053e1332e002c14e1d92935d423cf36
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-16 08:18:05 -0300

    A016: separating the code in specific files

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,19 @@
+const MissingParamError = require("./missing-prams-error");
+
+module.exports = class HttpResponse {
+  static badRequest(paramName){
+    return {
+        statusCode: 400,
+        body: new MissingParamError(paramName)
+      }
+  }
+  static internalError(){
+    return {
+        statusCode: 500
+      }
+  }static success(){
+    return {
+        statusCode: 200
+      }
+  }
+}
--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,47 +1,6 @@
-class LoginRouter{
-  route (httpRequest) {
-    if(!httpRequest || !httpRequest.body){
-      return HttpResponse.internalError();
-    }
+const LoginRouter = require("./login-router");
+const MissingParamError = require("../helpers/missing-prams-error");
 
-    const {email, password } = httpRequest.body
-
-    if(!email){
-      return HttpResponse.badRequest('email');
-    }
-    if(!password){
-      return HttpResponse.badRequest('password');
-    }
-
-    return HttpResponse.success();
-  }
-}
-
-
-class HttpResponse {
-  static badRequest(paramName){
-    return {
-        statusCode: 400,
-        body: new MissingParamError(paramName)
-      }
-  }
-  static internalError(){
-    return {
-        statusCode: 500
-      }
-  }static success(){
-    return {
-        statusCode: 200
-      }
-  }
-}
-
-class MissingParamError extends Error {
-  constructor (paramName){
-    super(`Missing param: ${paramName}`)
-    this.name = 'MissingParamError'
-  }
-}
 describe('Login Router', () => {
   test('should return 400 on no email provided', () => {
     const sut = new LoginRouter()
