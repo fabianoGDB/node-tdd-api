ac7bdc7c81cbdaf751c7fa31075b091096b3d60f
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-18 20:12:11 -0300

    A027: ensure to throw the specific error

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,14 +1,16 @@
+const { MissingParamError } = require("../../utils/erros");
 class AuthUseCase {
   async auth(email) {
     if (!email) {
-      throw new Error();
+      throw new MissingParamError("email");
     }
   }
 }
+
 describe("Auth UseCase", () => {
   test("should throw if no email isn't provided", async () => {
     const sut = new AuthUseCase();
     const promise = sut.auth("");
-    expect(promise).rejects.toThrow();
+    expect(promise).rejects.toThrow(new MissingParamError("email"));
   });
 });
