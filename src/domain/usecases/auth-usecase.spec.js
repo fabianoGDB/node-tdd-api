b05959a3a76cdcd0fa71f35f78e912eddb21ad2c
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-18 20:17:58 -0300

    A028: ensure password is provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,8 +1,11 @@
 const { MissingParamError } = require("../../utils/erros");
 class AuthUseCase {
-  async auth(email) {
+  async auth(email, password) {
     if (!email) {
       throw new MissingParamError("email");
+    }
+    if (!password) {
+      throw new MissingParamError("password");
     }
   }
 }
@@ -10,7 +13,13 @@ class AuthUseCase {
 describe("Auth UseCase", () => {
   test("should throw if no email isn't provided", async () => {
     const sut = new AuthUseCase();
-    const promise = sut.auth("");
+    const promise = sut.auth();
     expect(promise).rejects.toThrow(new MissingParamError("email"));
+  });
+
+  test("should throw if no password isn't provided", async () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth("any_email@mail.com");
+    expect(promise).rejects.toThrow(new MissingParamError("password"));
   });
 });
