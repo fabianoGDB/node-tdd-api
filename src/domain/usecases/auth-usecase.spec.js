c42b4ea38ae366f207f071065ad6b4ccb55ffaf4
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-18 19:56:27 -0300

    A025: ensure AuthUseCase throws if no email provided

---
+++ src/domain/usecases/auth-usecase.spec.js
@@ -0,0 +1,14 @@
+class AuthUseCase {
+  async auth(email) {
+    if (!email) {
+      throw new Error();
+    }
+  }
+}
+describe("Auth UseCase", () => {
+  test("should return null if no email isn't provided", async () => {
+    const sut = new AuthUseCase();
+    const promise = sut.auth("");
+    expect(promise).rejects.toThrow();
+  });
+});
