f2d38506b5af6380d0ee3db0eabfb2d5eb9cfad9
fabianoGDB <doctorfabi.f@gmail.com>
2025-07-21 21:30:43 -0300

    A038: ensure return of the accessToken with correct credentials

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -16,16 +16,15 @@ module.exports = class AuthUseCase {
     }
 
     const user = await this.loadUserByEmailRepository.load(email);
-    if (!user) {
-      return null;
+    const isValid =
+      user && (await this.encrypter.compare(password, user.password));
+
+    if (isValid) {
+      const accessToken = await this.tokenGenerator.generate(user.id);
+      return accessToken;
     }
 
-    const isValid = await this.encrypter.compare(password, user.password);
-    if (!isValid) {
-      return null;
-    }
-
-    await this.tokenGenerator.generate(user.id);
+    return null;
   }
 };
 
